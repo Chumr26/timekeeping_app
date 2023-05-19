6342fef9c04c2b552fcc6230916e5b04335fb9c9
khoa <nguyenanhkhoa26092002@gmail.com>
2023-05-19 21:39:39 +0700

    false adding filter

--- models/timekeeping_table.py
+++ models/timekeeping_table.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 class Timekeeping(models.Model):
     _name = "timekeeping.table"
@@ -10,21 +10,9 @@ class Timekeeping(models.Model):
         "product.product", string="Product", required=True)
     quantity = fields.Integer(string="Quantity")
     date = fields.Date(default=lambda self: fields.Date.today())
-    pay = fields.Float(string="Pay", compute="_compute_pay",store=True)   
+    pay = fields.Float(string="Pay", compute="_compute_pay", store=True)
 
-    @api.depends("product_id.list_price","quantity")
+    @api.depends("product_id.list_price", "quantity")
     def _compute_pay(self):
         for product in self:
             product.pay = product.product_id.list_price * product.quantity
-
-    # @api.model
-    # def _search_this_week(self, operator, value):
-    #     current_date = fields.Date.today()
-    #     start_of_week = current_date - timedelta(days=current_date.weekday())
-    #     end_of_week = start_of_week + timedelta(days=6)
-    #     return [('date', '>=', start_of_week.strftime('%Y-%m-%d')), ('date', '<=', end_of_week.strftime('%Y-%m-%d'))]
-    
-    # # Register the search filter
-    # _searchable = [
-    #     (_search_this_week, 'This Week'),
-    # ]
