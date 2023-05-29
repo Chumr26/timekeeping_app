2de6e24674753aa083ae2b08a32249cb36c4b34f
khoa <nguyenanhkhoa26092002@gmail.com>
2023-05-29 15:02:55 +0700

    removing the tree view in timekeeping.many model

--- models/timekeeping_many.py
+++ models/timekeeping_many.py
@@ -5,9 +5,6 @@ class Many(models.Model):
     _name = "timekeeping.many"
     _description = "Timekeeping Many"
 
-    date = fields.Date(
-        default= fields.Date.today()
-    )
     line_ids = fields.One2many(
         "timekeeping.table",
         "worker_id",
