14795a85a251b2c232304f2afcc38d2057205b54
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2023-08-18 16:10:58 +0700

    thêm filter 'hôm qua' và 'năm nay'

--- models/timekeeping_table.py
+++ models/timekeeping_table.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api  # type:ignore
 from odoo.exceptions import ValidationError  # type:ignore
-import logging
+from datetime import timedelta
 
 
 class Timekeeping(models.Model):
@@ -50,11 +50,15 @@ class Timekeeping(models.Model):
         compute="_compute_date_filter",
         store=True
     )
-    # field này dùng cho filter
-    # year = fields.Integer(
-    #     compute="_compute_date_filter",
-    #     store=True
-    # )
+    yesterday = fields.Date(
+        compute="_compute_date_filter",
+        store=True
+    )
+    current_year = fields.Integer(
+        compute="_compute_date_filter",
+        store=True
+    )
+    # 
     pay = fields.Float(
         compute="_compute_pay",
         store=True,
@@ -110,61 +114,6 @@ class Timekeeping(models.Model):
         for product in self:
             product.pay = product.order_line_id.product_id.list_price * product.quantity
 
-    # update quantity onhand
-    # @api.onchange("quantity")
-    # def _onchange_quantity(self):
-    #     logging.critical(f"có được gọi khi record được tạo{self.employee_id.name}, {self.quantity}")
-        # # nếu sản phẩm được nhập
-        # if self.order_line_id and self.employee_id.name:
-        #     logging.critical(f"{self.employee_id.name}qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqquantity{self.quantity}")
-        #     quant = self.env["stock.quant"].search(
-        #         [("product_id", "=", self.order_line_id.product_id.id)], limit=1)
-        #     # nếu record đã được lưu và tìm được số lượng của sản phẩm đó
-        #     if self._origin.id and quant:
-        #         total_quantity = quant.quantity - self._origin.quantity + self.quantity
-        #         quant.write({"quantity": total_quantity})
-        #     # nếu record lần đầu được tạo
-        #     elif quant:
-        #         total_quantity = quant.quantity + self.quantity
-        #         quant.write({"quantity": total_quantity})
-        #     # nếu record số lượng chưa được tạo
-        #     else:
-        #         self.env["stock.quant"].create({
-        #             "product_id": self.order_line_id.product_id.id,
-        #             "quantity": self.quantity,
-        #             "location_id": self.location_id.id
-        #         })
-
-
-    # khi đổi tên mã hàng
-    # @api.onchange("order_line_id")
-    # def _onchange_order_line_id(self):
-    #     logging.critical(f"có được gọi khi record được tạo{self.employee_id.name}, {self.quantity}")
-        # if self.order_line_id and self.employee_id.name:
-        #     logging.critical("ooooooooooooooooooooooooooooorder_line_id")
-        #     # tìm mã hàng trước đó
-        #     pr_product = self.env["stock.quant"].search(
-        #         [("product_id", "=", self._origin.order_line_id.product_id.id)], limit=1)
-        #     # trừ số lượng được nhập của mã hàng trước đó
-        #     if pr_product:
-        #         pr_product_quantity = pr_product.quantity - self._origin.quantity
-        #         pr_product.write({"quantity": pr_product_quantity})
-
-
-        #     # tìm mã hàng hiện tại
-        #     cr_product = self.env["stock.quant"].search(
-        #         [("product_id", "=", self.order_line_id.product_id.id)], limit=1)
-        #     # cập nhật số lượng cho mã hàng hiện tại
-        #     if cr_product:
-        #         cr_product_quantity = cr_product.quantity + self.quantity
-        #         cr_product.write({"quantity": cr_product_quantity})
-        #     else:
-        #         self.env["stock.quant"].create({
-        #             "product_id": self.order_line_id.product_id.id,
-        #             "quantity": self.quantity,
-        #             "location_id": self.location_id.id
-        #         })
-
     @api.onchange('order_id')
     def _onchange_order_id(self):
         # Clear the values of dependent fields
@@ -185,9 +134,8 @@ class Timekeeping(models.Model):
     def _compute_date_filter(self):
         for rc in self:
             rc.quarter = (rc.date.month - 1) // 3 + 1
-            # rc.year = rc.date.year
-
-    
+            rc.yesterday = rc.date - timedelta(days=1)
+            rc.current_year = rc.date.year
 
     @api.model
     def create(self, vals):
